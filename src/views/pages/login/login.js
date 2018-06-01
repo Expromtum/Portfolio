e6e895254f3edf4650c945cade4e22ebec4fc45f
Marina Sokurenko <stagger@mail.ru>
2018-06-01 15:29:16 +0300

    Верстка Авторизация2

--- src/views/pages/login/login.js
+++ src/views/pages/login/login.js
@@ -1,4 +1,7 @@
-//scss
+//scss common
 import 'src/styles/main.scss';
-import 'src/styles/blocks/btn_group.scss';
+// import 'src/styles/layout/text_settings';
+// import 'src/styles/blocks/btn_group.scss';
+
+//scss page
 import 'src/views/pages/login/login.scss';
