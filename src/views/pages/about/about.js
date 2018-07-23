a0a976b8773b20532152715e3d9cda57b8f44b19
Marina Sokurenko <stagger@mail.ru>
2018-07-23 16:03:45 +0300

    Верстка Skills Текст

--- src/views/pages/about/about.js
+++ src/views/pages/about/about.js
@@ -4,6 +4,8 @@ import 'src/views/pages/about/about.scss';
 import 'src/styles/blocks/hero.scss';
 import 'src/styles/blocks/user.scss';
 import 'src/styles/blocks/contacts.scss';
+import 'src/styles/blocks/hero.scss';
+import 'src/styles/blocks/skill.scss';
 
 //js
 import 'src/js/hero.js';
